91ed11f0c29f936a250d7045891316ca5a0a0467
Ramiro Tician <ramiro@tician.dev>
2022-06-30 09:47:15 -0300

    Changed command name to start web app.

--- cmd/service.go
+++ cmd/service.go
@@ -11,8 +11,8 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var serviceCmd = &cobra.Command{
-	Use:   "yas",
-	Short: "CLI to run YAS app",
+	Use:   "web",
+	Short: "CLI to run YAS web app",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("calling service")
 		serviceStart()
